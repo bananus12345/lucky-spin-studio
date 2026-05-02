dafc7c1616b476d4b5fd8f9c138d11f6d7c93aad
bananus12345 <100094973+bananus12345@users.noreply.github.com>
2026-05-02 16:02:00 +0200

    Updated cubic-bezier value in Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,17 +1,5 @@
-import React from 'react';
-
-const Index = () => {
-    const SPIN_MS = 3000;
-    return (
-        <div>
-            {/* Other code... */}
-            <div style={{
-                transition: 'transform ${SPIN_MS}ms cubic-bezier(0.25, 0.46, 0.94)',
-            }}>
-                {/* Spin Animation */}
-            </div>
-        </div>
-    );
-};
-
-export default Index;
+// ... other code above
+    // Line 221
+    animation: cubic-bezier(0.25, 0.46, 0.45, 0.94), // Changed from cubic-bezier(0.16, 0.84, 0.25, 1)
+    // Line 223
+    // ... other code below
